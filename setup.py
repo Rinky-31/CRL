b4996f4af6fb184855e722d188f2915725962e61
Rinky-31 <didorenkovlad1@gmail.com>
2024-10-31 18:00:49 +0200

    change setup.py. Add find_packages

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
     name="crlib",
     version="0.4.2",
     author="Vlad",
+    packages=find_packages(),
     extras_require = {"balance": "chempy"},
     python_requires = ">=3.10.3"
 )
