f5302504a0408db4fff3c7c2cff68f3883a8d13b
Rinky-31 <didorenkovlad1@gmail.com>
2024-11-07 20:50:54 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name="crlib",
     version="0.4.2",
-    author="Vlad",
+    author="Rinky-31",
     packages=find_packages(),
     extras_require = {"balance": "chempy"},
     python_requires = ">=3.10.3"
